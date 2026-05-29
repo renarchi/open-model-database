843e58935c46526b83f024b95331a08f9aea8459
renarchi <159624970+renarchi@users.noreply.github.com>
2026-05-29 14:16:27 +0300

    fix: disable optimizeFonts to fix CI build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const nextConfig = {
     // output: 'export',
     reactStrictMode: true,
+    optimizeFonts: false,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
